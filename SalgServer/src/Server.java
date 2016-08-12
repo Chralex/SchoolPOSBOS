16af1ee5dcac779f7e6e4f085738e1aab52a3b45
Chralex <chralexnet@gmail.com>
2016-08-12 12:25:27 +0200

    Added serialization and deserialization example in server code

--- SalgServer/src/Server.java
+++ SalgServer/src/Server.java
@@ -1,5 +1,10 @@
 import java.io.*;
 import java.net.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import DatabaseModel.DatabaseConnection;
+import DatabaseModel.Tables.Product;
 
 public class Server {
 	
@@ -28,7 +33,55 @@ public class Server {
 	public static void Run() throws IOException
 	{
 		MyService = new ServerSocket(PortNumber);
-	
+		
+    	DatabaseConnection db;
+    	ArrayList<Product> products;
+    	
+    	/*Testing serialization and deserialization*/
+    	try  {
+    		db = new DatabaseConnection(true);
+    	}
+    	catch (SQLException exception) {
+    		System.out.println("Failed to connect to database - " + exception);
+    		return;
+    	}
+    	
+    	try {
+    		 products = db.select(Product.class);
+    	}
+    	catch (SQLException exception) {
+    		System.out.println("Failed to get products for client - " + exception);
+    		return;
+    	}
+    	
+    	FileOutputStream stream = new FileOutputStream("tmp/Product.ser"); // WHYYY DO YOU HAVE TO SERIALIZE IN THIS WAY JAVA; WHAT THE FUCK.
+    	
+		ObjectOutputStream objectStream = new ObjectOutputStream(stream);
+		objectStream.writeObject(products);
+		objectStream.close();
+		stream.close();
+		
+		FileInputStream fileInputStream = new FileInputStream("tmp/Product.ser"); // Java serialization still sucks dunkey dick.
+		ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+		
+		try {
+			ArrayList<Product> deserializedProduct = (ArrayList<Product>)objectInputStream.readObject();
+			
+			for (Product product : deserializedProduct) {
+				System.out.print(product);
+			}
+		} 
+		catch (ClassNotFoundException anExceptionThatShouldNeverBeThrown)
+		{
+			System.out.println(anExceptionThatShouldNeverBeThrown);
+			return;
+		}
+		
+		objectInputStream.close();
+		fileInputStream.close();
+		/*Testing ended*/
+		
+		// Next up send the individual OutputFileStream bytes to the client. And parse it there.
 		boolean Running = true;
 	    while(Running = true)
 	    {
