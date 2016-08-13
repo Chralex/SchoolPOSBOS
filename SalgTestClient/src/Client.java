71119579a82b57ff57db3993d0255aedabb500da
Chralex <chralexnet@gmail.com>
2016-08-13 02:03:59 +0200

    EH: Implemented deserialization example on client

--- SalgTestClient/src/Client.java
+++ SalgTestClient/src/Client.java
@@ -1,33 +1,25 @@
 import java.io.*;
 import java.net.*;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import DatabaseModel.Tables.Product;
+
 public class Client {
 
 public static int PortNumber = 2035;
 
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-		
-
-	    Socket MyClient = new Socket("Localhost", PortNumber);
-	    InputStream obj = MyClient.getInputStream();
+	    Socket socket = new Socket("Localhost", PortNumber);
 	    
-	    
-
-	    
-	    BufferedReader br = new BufferedReader(new InputStreamReader(obj));
-	    
-	    
-	    
-	    
-	    String str;
-	    
-	    while((str=br.readLine())!=null){
-	    	System.out.println("From Server: " + str);
+	    if (socket.isConnected()) {	    
+		    ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+		    ArrayList<Product> products = (ArrayList<Product>)ois.readObject();
+		    System.out.println(products);
+		    ois.close();
 	    }
 	    
-	    br.close();
-	    MyClient.close();
-	    
+	    socket.close();
 	}
 
 }
