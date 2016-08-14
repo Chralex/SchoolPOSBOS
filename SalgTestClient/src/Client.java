7a8ad4855fbe174154f4be71214a6b4c9b7f3a26
Malthe Mathias Nørgaard Laustsen <Malthe Mathias Nrgaard Laustsen>
2016-08-14 15:28:19 +0200

    tillader kommandoer fra klient til server + if-else sætninger

--- SalgTestClient/src/Client.java
+++ SalgTestClient/src/Client.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import DatabaseModel.Tables.Product;
 
 public class Client {
@@ -11,13 +13,45 @@ public class Client {
 		// TODO Auto-generated method stub
 		Socket socket = new Socket("Localhost", PortNumber);
 
-		if (socket.isConnected()) {
+		PrintWriter pw;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Skriv kommando");
+		String input = scan.next();
+		scan.close();
+		
+		if (input.toLowerCase().trim().equals("getproducts") && socket.isConnected())
+		{
+			
+			
+			
+		//if (socket.isConnected()) {
+			
+		    pw = new PrintWriter(socket.getOutputStream(), true);
+		    pw.println(input);
+			
 			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
 			ArrayList<Product> products = (ArrayList<Product>) ois.readObject();
 			System.out.println(products);
+			//System.out.println(products.get(0).name);   // products.size();
 			ois.close();
+		//}
 		}
-
+		else if (input.toLowerCase().trim().equals("sendsales") && socket.isConnected())
+		{
+		    pw = new PrintWriter(socket.getOutputStream(), true);
+		    pw.println(input);
+		}
+		else if (input.toLowerCase().trim().equals("sendpurchase") && socket.isConnected())
+		{
+			 pw = new PrintWriter(socket.getOutputStream(), true);
+			 pw.println(input);
+		}
+		else{
+			
+			
+		}
 		socket.close();
 	}
 
