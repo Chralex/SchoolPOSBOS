8ea21fe7451f558dc424b6f1576c181341f9a35f
Chralex <chralexnet@gmail.com>
2016-08-15 16:36:17 +0200

    Removed unsused reference

--- SalgServer/src/Program.java
+++ SalgServer/src/Program.java
@@ -2,8 +2,6 @@ import java.sql.*;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.UUID;
-
 import Authentication.*;
 import DatabaseModel.*;
 import DatabaseModel.DatabaseInitializer.FieldNotInitializedException;
